d2a59c4121bdbcf3e7fb82f152a60b76795eb34e
Andrew Salcedo <asalcedo07@gmail.com>
2017-04-06 10:18:16 -0400

    add getMovie(); movie.html successfully retrieves omdb data

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-/* When document loads this function attaches a
+/*
+ * When document loads this function attaches a
  * click event handler when the form is submitted.
  * Grabs the searchText value. runs getAllMovies();
  * and prevent the submitting of a form to a file.
@@ -14,7 +15,7 @@ $(document).ready(() => {
 /*
  * Uses the value of searchText to retrieve movies from
  * the OMDB API usng Axios.
- * param: {string} SearchText - input text in #searchForm.
+ * @param: {string} SearchText - input text in #searchForm.
  */
 function getAllMovies(searchText) {
       axios.get('http://www.omdbapi.com/?s=' + searchText)
@@ -43,15 +44,59 @@ function getAllMovies(searchText) {
 /*
  * When movie is clicked on, the movie takes in the imdbID and passes
  * the API data to movie.html via local session storage.
-*/
+ * @param: {string} id - the imdbID of the movie.
+ */
 function movieSelected(id) {
     sessionStorage.setItem('movieId', id);
     window.location = 'movie.html';
-    return false; 
+    return false;
 }
 
-
-
+/*
+ * Function retrives movie data via axios & OMDB api.
+ * displays data from the id associated with movieSelected()
+ * displays movie info on movie.html
+ */
 function getMovie() {
+    let movieId = sessionStorage.getItem('movieId');
 
+    axios.get('http://www.omdbapi.com/?i=' + movieId)
+      .then((response) => {
+          console.log(response);
+          let movie = response.data;
+
+          let output = `
+                <div class="row">
+                  <div class="col-md-4">
+                      <img src="${movie.Poster}" class="thumbnail">
+                  </div>
+                  <div class="col-md-8">
+                      <h2>${movie.Title}</h2>
+                      <ul class="list-group">
+                          <li class="list-group-item"> <strong>Genre:</strong> ${movie.Genre}</li>
+                          <li class="list-group-item"> <strong>Released:</strong> ${movie.Released}</li>
+                          <li class="list-group-item"> <strong>Rated:</strong> ${movie.Rated}</li>
+                          <li class="list-group-item"> <strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+                          <li class="list-group-item"> <strong>Actors:</strong> ${movie.Actors}</li>
+                          <li class="list-group-item"> <strong>Director:</strong> ${movie.Director}</li>
+                          <li class="list-group-item"> <strong>Runtime:</strong> ${movie.Runtime}</li>
+                      </ul>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="well">
+                    <h3>Plot</h3>
+                    ${movie.Plot}
+                    <hr>
+                    <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+                    <a href="index.html" class="btn btn-info">Go back to Search</a>
+                  </div>
+                </div>
+          `;
+
+          $('#movie').html(output);
+      })
+      .catch((error) => {
+          console.log("No imdb info");
+      });
 }
